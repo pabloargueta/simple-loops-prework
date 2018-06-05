5d69d2ec45a3d6385fd4cc5256761c972ad1c682
Pablo Argueta <pabloargueta27@gmail.com>
2018-06-05 06:32:21 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,7 +37,7 @@ def while_iterator(number_of_times)
   
   while counter < number_of_times
   
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  puts "Welcome to Flatiron School's Web Development Course!"
   
   counter += 1
   
